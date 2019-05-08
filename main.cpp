db31a452b315c37fab479476ce1a769a47341531
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-05-08 18:48:05 -0500

    Double space

--- main.cpp
+++ main.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
+double mpg(double, double, double);
 
-using namespace std;
+
 
 int main (){
-	
-	int liter, mile, mpg;
-	double unit = 0.26179;
+	using namespace std;
+	double unit = 0.26179, liter, mile;
 	char again = 'Y';
+	
+	cout.setf(ios::fixed);
+	cout.setf(ios::showpoint);
+	cout.precision(2);
+	
 	do{
 		
 	cout << "Enter the number of liters of gasoline consumed and the miles traveled" << endl;
 	cin >> liter >> mile;
-	mpg = mile / (liter * unit);
-	cout << "Your car had done " << mpg << " miles per gallon." << endl;
+	// mpg = mile / (liter * unit);
+	cout << "Your car had done " << mpg (liter, mile, unit) << " miles per gallon." << endl;
 	cout << "Do you want to try again? Y/n" << endl;
 	cin >> again;
 	} while (again == 'y' || again == 'Y');
@@ -20,3 +25,6 @@ int main (){
 	return 0;
 }
 
+double mpg (double liter, double mile, double unit){
+	return mile / (liter * unit);
+}
